2ddac538e89b18e40dddad50c24a1ad7cf2ebb5c
Dan Conrad <dan.m.conrad@gmail.com>
2017-06-10 16:34:13 +0000

    feat: initial commit

---
+++ coffee.py
@@ -0,0 +1,9 @@
+from papirus import PapirusText
+
+sans_font="/usr/share/fonts/truetype/freefont/FreeSans.ttf"
+
+text = PapirusText()
+
+# Write text to the screen
+# text.write(text)
+text.write("omg, boops, it' so cute!", font_path=sans_font)
